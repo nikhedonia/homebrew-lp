8198a6a8d9dd7bc8e9daa7f1cbd00e67ac2628f0
Gaetano Checinski <Gaetano.Checinski@gmail.com>
2017-04-25 13:30:36 +0100

    Update buckaroo.rb

--- buckaroo.rb
+++ buckaroo.rb
@@ -4,7 +4,6 @@ class Buckaroo < Formula
   homepage 'https://github.com/loopperfect/buckaroo'
   head 'git@github.com:loopperfect/buckaroo.git', :using => :git
   depends_on :java => "1.8+"
-  depends_on 'buck'
   def install
     system 'buck', 'build', ':buckaroo-cli'
     libexec.install Dir["buck-out/gen/*"]
